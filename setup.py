0c1dbea17482b5a4fa7c5fd17201b341989bc7f6
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2020-07-24 16:23:51 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='rf_event_listener',
-    version='0.0.2',
+    version='0.0.3',
     description='RedForester Event Listener',
     long_description=long_description,
     long_description_content_type='text/markdown',
